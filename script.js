215ccc69b07e787ee2f204b2a1ab67032bb45e15
dyoder838 <70074229+dyoder838@users.noreply.github.com>
2020-09-10 22:23:02 -0700

    added a console.log to trouble shoot for loop

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
+//DOM Objects
+// we have 2 items in the html that we are concerned with: the button, and the text area
 // this is the variable for button
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
+// this is the variable for textarea
+const passwordText = document.querySelector("#password");
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbols,
+
+}
 // Write password to the #password input
 // function writePassword() {
-//     var password = generatePassword();
-//     var passwordText = document.querySelector("#password");
+//     var password = generatePassword()
+
+// passwordText is where the password is out put in html
+
 
 //     passwordText.value = password;
 
@@ -33,37 +46,71 @@ generateBtn.addEventListener("click", () => {
  
 //3 user data is stored 
 
-    // TODO: create new let that holds chosen characters
+    // TODO: create new variable that holds chosen characters
     // this holds the users input for each confirm - 
-     = generatePassword(passwordLower, passwordUpper, passwordNumbers, passwordCharacter, passwordLength)
+    passwordText.innerText = generatePassword(
+        passwordLower, 
+        passwordUpper, 
+        passwordNumbers, 
+        passwordCharacter, 
+        passwordLength
+        );
 }); 
     
-
+////////////////end of button function///////////////////
 //4 user data gets turned into a password
 
-// this generates the password
+// this function generates the password using our 5 variables
 function generatePassword(lower, upper, number, symbol, length) {
-    // initialize password variable
-    // filter out false
-    // loop over length call generator function for each type
-    // add final pw to the pw variable and return
+    
+//TODO: initialize password variable
     let generatedPassword = "";
-
+    
+    // user data gets turned into an array so the computer can translate to index numbers.
+    // create a variable for the user input.
     const typesCount = lower + upper + number + symbol;
 
-    //use this to check for functionality, this should count how many times ok is pushed by the user
+    // use this to check for functionality, this should count how many times true was logged
     console.log("typesCount: ", typesCount);
-}
+
+//TODO: filter out false 
+    // turn the variable into a useable array, .filter item, gets rid of the false items... the things the user said no to 
+    const typesArr = [{lower}, {upper}, {number}, {symbol}].filter
+    (
+        item => Object.values(item)[0]
+    );
+        
+        //make sure the array is in the correct order - add curly brackets so console displays all of the object information
+        console.log("typesArr; ", typesArr);
+
+        // this is what happens when the user makes no selections
+        if(typesCount === 0) {
+            return "";
+        };
+ 
+//TODO: for loop to pick from the filtered array at random until the length variable has bees reached - calls from the 5 random generators
+        // this is the for loop that randomizes the array, represented here by the variable typesCount, note. length is used here as our 5th variable 
+        //i+= typesCount increments by the filtered array items i = 1 because of the array, if i=0 you would get an extra item
+        for (let i = 1; i< length; i+= typesCount) {
+            typesArr.forEach(type => {
+                //funcName holds the info from 4 of the 5 arrays upper, lower, number, symbol.
+                const funcName = Object.keys(type)[0];
+
+                // you result should be functions with the names lower, upper, number, symbol -- in the amount of characters in the length variable
+                console.log("funcName: ", funcName);
+
+                // taking the empty key - generatedPassword-; is a let variable at the top of the parent function that = "", and adding it to the function randomFunc - this assigns the result to the variable randomFunc
+                generatedPassword += randomFunc[funcName]();
+            });
+        }
+    }
+
+
+ //TODO: add final pw to the pw variable and return
 
         
     //function to hold the generators results
-        const randomFunc = {
-            lower: getRandomLower,
-            upper: getRandomUpper,
-            number: getRandomNumber,
-            symbol: getRandomSymbols,
-
-        }
+       
      // create 5 arrays - one for each requirement
         // lowercase generator function... (character map) http://net-comber.com/charset.html 26 is for letters in the alphabet , 97 is the reference to the index of the character code(what the index number of lowercase letters are - this creates a sub section of the existing array)** this list starts at spot 97, and proceeds at random up 26 index numbers(the amount of letters in the alphabet)
         function getRandomLower() {
